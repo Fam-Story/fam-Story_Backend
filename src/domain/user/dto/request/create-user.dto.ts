d4f989750afb412978071dfa74dbd4d7537ea09f
Ahn Jiwan <synoti21@naver.com>
2023-10-18 14:23:56 +0900

    :sparkles: feat: add create-user dto (#9)

--- src/domain/user/dto/request/create-user.dto.ts
+++ src/domain/user/dto/request/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nickname: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  age: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  gender: number;
+}
