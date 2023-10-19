799f87b172f9324c42d7270c8a33c3a82bec2a19
Ahn Jiwan <synoti21@naver.com>
2023-10-19 22:45:09 +0900

    :sparkles: feat: implement findByUserId() (#11)

--- src/domain/user/user.service.ts
+++ src/domain/user/user.service.ts
@@ -10,12 +10,12 @@ export class UserService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
-  async saveUser(createUserDto: CreateUserDto) {
-    const savedUser = await this.userRepository.save(createUserDto);
+  async saveUser(createUserDto: CreateUserDto): Promise<number> {
+    const savedUser: User = await this.userRepository.save(createUserDto);
     return savedUser.id;
   }
 
-  async updateUser(updateUserDto: UpdateUserDto) {
+  async updateUser(updateUserDto: UpdateUserDto): Promise<void> {
     const user = await this.userRepository.findOne({
       where: { id: updateUserDto.userId },
     });
@@ -24,5 +24,16 @@ export class UserService {
       throw new NotFoundException('해당 유저가 존재하지 않습니다.');
     }
     await this.userRepository.save(updateUserDto);
+  }
+
+  async findUserById(userId: number): Promise<User> {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new NotFoundException('해당 유저가 존재하지 않습니다.');
+    }
+    return user;
   }
 }
