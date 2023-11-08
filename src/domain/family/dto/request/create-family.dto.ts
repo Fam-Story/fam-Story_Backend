1d7b1e806fbdf028c4e3d515a0b249e001cdc238
Ahn Jiwan <synoti21@naver.com>
2023-11-08 20:44:31 +0900

    :fire: feat: CreateFamilyDto의 불필요한 name 속성 제거 (#23)

--- src/domain/family/dto/request/create-family.dto.ts
+++ src/domain/family/dto/request/create-family.dto.ts
@@ -1,19 +1,7 @@
-import { IsDate, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFamilyDto {
   @IsNotEmpty()
   @IsString()
-  name: string;
-
-  @IsNumber()
-  @IsString()
-  memberNumber: number;
-
-  @IsNotEmpty()
-  @IsString()
   familyName: string;
-
-  @IsNotEmpty()
-  @IsDate()
-  createdDate: Date;
 }
