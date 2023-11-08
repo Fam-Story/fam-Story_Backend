e2de4997aad135c7d7c95905adaba7e91558694c
Ahn Jiwan <synoti21@naver.com>
2023-11-08 19:48:21 +0900

    :sparkles: feat: 가족 고유 코드 생성 및 추가, 반환 구현 (#23)

--- src/domain/family/family.service.ts
+++ src/domain/family/family.service.ts
@@ -1,18 +1,58 @@
 import { Injectable } from '@nestjs/common';
-import { CreateFamilyDto, UpdateFamilyDto } from './dto';
+import * as crypto from 'crypto';
+import { CreateFamilyDto, ResponseFamilyDto } from './dto';
+import { Family, User } from '../../infra/entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { FamilyException } from '../../common/exception/family.exception';
+import { ResponseCode } from '../../common';
 
 @Injectable()
 export class FamilyService {
+  constructor(
+    @InjectRepository(User) private familyRepository: Repository<Family>,
+  ) {}
 
   //가족 고유 해쉬키 생성
+  createFamilyKeyCode(): string {
+    return crypto.randomBytes(10).toString('hex');
+  }
 
   //가족 엔티티 추가
+  async createFamily(createFamilyDto: CreateFamilyDto) {
+    const familyKeyCode = this.createFamilyKeyCode();
+    const family: Family = Family.createFamily(
+      createFamilyDto.familyName,
+      familyKeyCode,
+    );
 
-  //가족 엔티티 반환
+    const savedFamily = await this.familyRepository.save(family);
+    return savedFamily.id;
+  }
+
+  //가족 엔티티 반환 (id로)
+  async findFamilyById(familyId: number): Promise<ResponseFamilyDto> {
+    const family = await this.familyRepository.findOne({
+      where: { id: familyId },
+    });
+    if (!family) {
+      throw new FamilyException(ResponseCode.FAMILY_NOT_FOUND);
+    }
+    return ResponseFamilyDto.from(family);
+  }
+
+  //가족 엔티티 반환 (keyCode로)
+  async findFamilyByKeyCode(keyCode: string): Promise<ResponseFamilyDto> {
+    const family = await this.familyRepository.findOne({
+      where: { keyCode: keyCode },
+    });
+    if (!family) {
+      throw new FamilyException(ResponseCode.FAMILY_NOT_FOUND);
+    }
+    return ResponseFamilyDto.from(family);
+  }
 
   //가족 삭제
 
   //가족 정보 업데이트
-  
-
 }
