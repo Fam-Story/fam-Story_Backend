23a6f1e897bc01d91c178bb4c5afd2eefc74db7e
Ahn Jiwan <synoti21@naver.com>
2023-11-11 21:42:41 +0900

    :white_check_mark: test: Family Controller e2e 테스트 구현 (#25)

--- src/test/e2e/family.controller.spec.ts
+++ src/test/e2e/family.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { FamilyService, ResponseFamilyDto } from '../../domain/family';
+import {
+  FamilyService,
+  ResponseFamilyDto,
+} from '../../domain/family';
 import { INestApplication } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Family } from '../../infra/entities';
@@ -18,10 +21,17 @@ describe('FamilyController (e2e)', () => {
       findFamilyById: jest
         .fn()
         .mockResolvedValue(ResponseFamilyDto.from(mockFamily)),
+      findFamilyByKeyCode: jest
+        .fn()
+        .mockResolvedValue(ResponseFamilyDto.from(mockFamily)),
       validateFamily: jest.fn().mockResolvedValue(mockFamily),
+      createFamily: jest.fn().mockResolvedValue(1),
+      deleteFamily: jest.fn(),
+      updateFamily: jest.fn(),
     };
 
     mockFamilyRepository = {
+      findOne: jest.fn().mockResolvedValue(mockFamily),
       find: jest.fn(),
     };
 
@@ -46,5 +56,42 @@ describe('FamilyController (e2e)', () => {
     expect(response.body.message).toEqual('가족 조회 성공');
     expect(response.body.data.familyName).toEqual('test');
     expect(response.body.data.familyKeyCode).toEqual('testKeyCode');
+  });
+
+  it('should return family info with path: /family/keycode (GET)', async () => {
+    const response = await request(app.getHttpServer())
+      .get('/family/invite/testKeyCode')
+      .expect(200);
+
+    expect(response.body.message).toEqual('가족 조회 성공');
+    expect(response.body.data.familyName).toEqual('test');
+    expect(response.body.data.familyKeyCode).toEqual('testKeyCode');
+  });
+
+  it('should return id of created family with path: /family/create (POST)', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/family/create')
+      .send({ familyName: 'test' })
+      .expect(201);
+
+    expect(response.body.message).toEqual('가족 생성 성공');
+    expect(response.body.data).toEqual(1);
+  });
+
+  it('should return status code 200 when update family', async () => {
+    const response = await request(app.getHttpServer())
+      .patch('/family/update')
+      .send({ familyId: 1, familyName: 'test' })
+      .expect(200);
+
+    expect(response.body.message).toEqual('가족 정보 수정 성공');
+  });
+
+  it('should return status code 200 when delete family', async () => {
+    const response = await request(app.getHttpServer())
+      .delete('/family/delete/1')
+      .expect(200);
+
+    expect(response.body.message).toEqual('가족 삭제 성공');
   });
 });
