167c79afd17c36d7bcf2c8033c3db6a5f90c98e0
Ahn Jiwan <synoti21@naver.com>
2023-11-15 22:58:40 +0900

    :memo: docs: dto에 swagger 문서 내용 property 추가 (#28)

--- src/domain/family-member/dto/request/create-family-member.dto.ts
+++ src/domain/family-member/dto/request/create-family-member.dto.ts
@@ -1,14 +1,21 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateFamilyMemberDto {
+  @ApiProperty({ example: 1, description: '유저 고유 ID' })
   @IsNotEmpty()
   @IsNumber()
   readonly userId: number;
 
+  @ApiProperty({ example: 1, description: '가족 고유 ID' })
   @IsNotEmpty()
   @IsNumber()
   readonly familyId: number;
 
+  @ApiProperty({
+    example: 1,
+    description: '가족 멤버의 역할, (1: 부, 2: 모, 3: 아들, 4: 딸)',
+  })
   @IsNotEmpty()
   @IsNumber()
   readonly role: number;
