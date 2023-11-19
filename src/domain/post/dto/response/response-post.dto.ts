4a632831b36a5ce5e00db1335bbae26009b853f2
Ahn Jiwan <synoti21@naver.com>
2023-11-19 15:58:00 +0900

    :bug: fix: 잘못된 post 응답 dto 수정 (#32)

--- src/domain/post/dto/response/response-post.dto.ts
+++ src/domain/post/dto/response/response-post.dto.ts
@@ -1,8 +1,23 @@
+import { Post } from '../../../../infra/entities';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class ResponsePostDto {
+  @ApiProperty({ example: 1, description: '포스트의 고유 ID' })
   readonly postId: number;
-  readonly familyId: number;
-  readonly s3ImageUrl: string;
-  readonly name: string;
+
+  @ApiProperty({
+    example: 1,
+    description: '포스트를 작성한 가족 구성원의 고유 ID',
+  })
+  readonly familyMemberId: number;
+
+  @ApiProperty({ example: '푸앙이', description: '포스트의 제목' })
+  readonly title: string;
+
+  @ApiProperty({ example: '푸앙이가 먹는 모습', description: '포스트의 내용' })
+  readonly context: string;
+
+  @ApiProperty({ example: '2021-10-10', description: '포스트가 작성된 날짜' })
   readonly createdDate: Date;
 
   private constructor(
@@ -13,19 +28,29 @@ export class ResponsePostDto {
     createdDate: Date,
   ) {
     this.postId = postId;
-    this.familyId = familyId;
-    this.s3ImageUrl = s3ImageUrl;
-    this.name = name;
+    this.familyMemberId = familyId;
+    this.title = s3ImageUrl;
+    this.context = name;
     this.createdDate = createdDate;
   }
 
   static of(
     postId: number,
     familyId: number,
-    s3ImageUrl: string,
-    name: string,
+    title: string,
+    context: string,
     createdDate: Date,
   ): ResponsePostDto {
-    return new ResponsePostDto(postId, familyId, s3ImageUrl, name, createdDate);
+    return new ResponsePostDto(postId, familyId, title, context, createdDate);
+  }
+
+  static from(post: Post) {
+    return ResponsePostDto.of(
+      post.id,
+      post.srcMember.family.id,
+      post.title,
+      post.context,
+      post.createdDate,
+    );
   }
 }
