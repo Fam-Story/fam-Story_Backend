9ba01f0212c9ae4cbeef47dbd0f4a53d2f8af868
Ahn Jiwan <synoti21@naver.com>
2023-11-22 01:26:18 +0900

    :recycle: refactor: Sonarqube를 고려한 코드 리팩토링 (#49)

--- src/common/exception/family-member.exception.ts
+++ src/common/exception/family-member.exception.ts
@@ -1,8 +1,4 @@
 import { ResponseCode } from '../api';
 import { BaseException } from './base.exeception';
 
-export class FamilyMemberException extends BaseException {
-  constructor(responseCode: ResponseCode) {
-    super(responseCode);
-  }
-}
+export class FamilyMemberException extends BaseException {}
--- src/common/exception/user.exception.ts
+++ src/common/exception/user.exception.ts
@@ -1,8 +1,4 @@
 import { ResponseCode } from '../api';
 import { BaseException } from './base.exeception';
 
-export class UserException extends BaseException {
-  constructor(responseCode: ResponseCode) {
-    super(responseCode);
-  }
-}
+export class UserException extends BaseException {}
