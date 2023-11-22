a7491a0ce7827b1c8119ef41eeec118ad19184ab
Ahn Jiwan <synoti21@naver.com>
2023-11-22 18:55:56 +0900

    :memo: docs: photo api 명세서 보강 (#49)

--- src/domain/photo/photo.controller.ts
+++ src/domain/photo/photo.controller.ts
@@ -8,10 +8,11 @@ import {
   Query,
   UseGuards,
 } from '@nestjs/common';
-import { CreatePhotoDto, PhotoService, UpdatePhotoDto } from '../photo';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {CreatePhotoDto, PhotoService, ResponsePhotoDto, UpdatePhotoDto} from '../photo';
+import {ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags} from '@nestjs/swagger';
 import { CustomApiResponse, ResponseCode } from '../../common';
 import { JwtServiceAuthGuard } from '../../auth/guards/jwt-service-auth.guard';
+import {CustomApiOKResponse} from "../../common/api/response-ok.decorator";
 
 @ApiTags('사진 API')
 @Controller('photo')
@@ -26,6 +27,10 @@ export class PhotoController {
     summary: '[사진] 사진 업로드',
     description: '사진을 업로드한다.',
   })
+  @CustomApiOKResponse(
+    Number,
+    '사진 업로드를 성공하면 Status Code 201과 photoId를 반환한다.',
+  )
   async createPhoto(@Body() createPhotoDto: CreatePhotoDto) {
     const photoId = await this.photoService.createPhoto(createPhotoDto);
     return CustomApiResponse.success(
@@ -40,6 +45,7 @@ export class PhotoController {
     summary: '[사진] 사진 삭제',
     description: '사진을 삭제한다.',
   })
+  @CustomApiOKResponse(null, '사진 삭제 성공할 시, Status Code 200를 반환한다')
   async deletePhoto(@Query('photoId') photoId: number) {
     await this.photoService.deletePhoto(photoId);
     return CustomApiResponse.success(ResponseCode.PHOTO_DELETE_SUCCESS, null);
@@ -48,9 +54,10 @@ export class PhotoController {
   //사진 수정
   @Put('')
   @ApiOperation({
-    summary: '[사진] 사진 수정',
-    description: '사진을 수정한다.',
+    summary: '[사진] 사진 정보 수정',
+    description: '사진의 정보 수정한다.',
   })
+  @CustomApiOKResponse(null, '사진의 정보를 수정할 시, Status Code 200 반환')
   async updatePhoto(@Body() updatePhotoDto: UpdatePhotoDto) {
     await this.photoService.updatePhotoInfo(updatePhotoDto);
     return CustomApiResponse.success(ResponseCode.PHOTO_UPDATE_SUCCESS, null);
@@ -63,6 +70,7 @@ export class PhotoController {
     description:
       '가족의 모든 사진을 반환한다. 가족 Id와 페이지, 한 페이지당 사진 개수를 Query parameter로 받는다.',
   })
+  @CustomApiOKResponse(ResponsePhotoDto, 'ResponsePhotoDto의 배열을 반환한다.')
   async getPhotos(
     @Query('familyId') familyId: number,
     @Query('page') page: number,
