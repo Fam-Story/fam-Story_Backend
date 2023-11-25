dcd7c87d7e47f3d1651dbaaa23d83303c892846d
Ahn Jiwan <synoti21@naver.com>
2023-11-25 23:35:19 +0900

    :sparkles: feat: fcm Handler 구현 (#49)

--- src/common/util/fcm-handler.ts
+++ src/common/util/fcm-handler.ts
@@ -1,30 +1,43 @@
-// import { Injectable } from '@nestjs/common';
-// import * as firebase from 'firebase-admin';
-// import * as path from 'path';
-//
-// firebase.initializeApp({
-//   credential: firebase.credential.cert(
-//     path.join(__dirname, '..', '..', 'firebase-adminsdk.json'),
-//   ),
-// });
-//
-// @Injectable()
-// export class FirebaseCloudMessagingHandler {
-//   acceptPushNotification = async (
-//     user: any,
-//     notification_dto: NotificationDto,
-//   ): Promise<NotificationToken> => {};
-//
-//   disablePushNotification = async (
-//     user: any,
-//     update_dto: UpdateNotificationDto,
-//   ): Promise<void> => {};
-//
-//   getNotifications = async (): Promise<any> => {};
-//
-//   sendPush = async (
-//     user: any,
-//     title: string,
-//     body: string,
-//   ): Promise<void> => {};
-// }
+import { Injectable } from '@nestjs/common';
+import * as firebaseConfig from './firebase-admin.json';
+import * as firebase from 'firebase-admin';
+
+const firebase_params = {
+  type: firebaseConfig.type,
+  projectId: firebaseConfig.project_id,
+  privateKeyId: firebaseConfig.private_key_id,
+  privateKey: firebaseConfig.private_key,
+  clientEmail: firebaseConfig.client_email,
+  clientId: firebaseConfig.client_id,
+  authUri: firebaseConfig.auth_uri,
+  tokenUri: firebaseConfig.token_uri,
+  authProviderX509CertUrl: firebaseConfig.auth_provider_x509_cert_url,
+  clientC509CertUrl: firebaseConfig.client_x509_cert_url,
+};
+
+@Injectable()
+export class FirebaseCloudMessagingHandler {
+  constructor() {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(firebase_params),
+    });
+  }
+  async sendNotification(token: string, title: string, body: string) {
+    const message = {
+      notification: {
+        title: title,
+        body: body,
+      },
+      token: token,
+    };
+    return await firebase
+      .messaging()
+      .send(message)
+      .then((response) => {
+        return response;
+      })
+      .catch((error) => {
+        return error;
+      });
+  }
+}
