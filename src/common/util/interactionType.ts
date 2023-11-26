e37c426da62886c633a9fbafd366e47584e30aea
Ahn Jiwan <synoti21@naver.com>
2023-11-26 20:38:59 +0900

    :pencil2: fix: Interaction Type 오탈자 수정 (#49)

--- src/common/util/interactionType.ts
+++ src/common/util/interactionType.ts
@@ -1,4 +1,4 @@
-export class InteractIionType {
+export class InteractionType {
   title: string;
   body: string;
 
@@ -10,22 +10,22 @@ export class InteractIionType {
   static getInteraction(type: number, source: string, target: string) {
     switch (type) {
       case 1:
-        return new InteractIionType(
+        return new InteractionType(
           '팔로우',
           `${source}님이 ${target}님을 팔로우 하셨습니다.`,
         );
       case 2:
-        return new InteractIionType(
+        return new InteractionType(
           '찌르기',
           `${source}님이 ${target}님을 찌르셨습니다.`,
         );
       case 3:
-        return new InteractIionType(
+        return new InteractionType(
           '좋아요',
           `${source}님이 ${target}님의 게시물을 좋아합니다.`,
         );
       case 4:
-        return new InteractIionType(
+        return new InteractionType(
           '댓글',
           `${source}님이 ${target}님의 게시물에 댓글을 남겼습니다.`,
         );
