e94da7559d1c7b47d6c6924086526d7ef084bb48
Ahn Jiwan <synoti21@naver.com>
2023-11-30 23:51:50 +0900

    :white_check_mark: test: 채팅 unit test 틀 구현 (#66)

--- src/test/service/chat.service.spec.ts
+++ src/test/service/chat.service.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ChatService } from './chat.service';
+import { ChatService } from '../../chat/chat.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ChatMessage } from '../../infra/entities/message.entity';
+import { FamilyMember } from '../../infra/entities';
 
 describe('ChatService', () => {
+  const mockRepository = () => ({
+    create: jest.fn(),
+    save: jest.fn(),
+    find: jest.fn(),
+    delete: jest.fn(),
+  });
+
   let service: ChatService;
+  let chatRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
+      providers: [
+        ChatService,
+        {
+          provide: getRepositoryToken(ChatMessage),
+          useFactory: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(FamilyMember),
+          useFactory: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ChatService>(ChatService);
+    chatRepository = module.get(getRepositoryToken(ChatMessage));
   });
 
   it('should be defined', () => {
