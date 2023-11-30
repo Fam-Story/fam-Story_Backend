96ef05b689bd06af67948c1f77713b28b38e79e3
Ahn Jiwan <synoti21@naver.com>
2023-11-30 10:26:47 +0900

    :recycle: refactor: eslint 적용 (#57)

--- src/module/post.module.ts
+++ src/module/post.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PostController, PostService } from '../domain/post';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import {Family, FamilyMember, Post} from '../infra/entities';
+import { Family, FamilyMember, Post } from '../infra/entities';
 
 @Module({
   imports: [TypeOrmModule.forFeature([FamilyMember, Family, Post])],
