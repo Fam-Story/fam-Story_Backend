6399241d394d84c6a3d118232b5ffd7c52abb31c
Ahn Jiwan <synoti21@naver.com>
2023-12-03 19:58:38 +0900

    :memo: docs: 채팅 관련 dto Swagger 문서화 (#66)

--- src/domain/chat/dto/create-chat.dto.ts
+++ src/domain/chat/dto/create-chat.dto.ts
@@ -1,5 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateChatDto {
+  @ApiProperty({
+    example: '1',
+    description: '가족 ID',
+  })
   readonly familyId: string;
+  @ApiProperty({
+    example: '1',
+    description: '가족 구성원 ID',
+  })
   readonly familyMemberId: string;
+  @ApiProperty({
+    example: '안녕하세요',
+    description: '채팅 내용',
+  })
   readonly message: string;
 }
