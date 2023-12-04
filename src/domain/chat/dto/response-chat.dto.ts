b153a1f60125a74e6fc75cb958dbd59a225de744
Ahn Jiwan <synoti21@naver.com>
2023-12-04 22:14:23 +0900

    :sparkles: feat: 채팅 응답 dto 변경 (#72)

--- src/domain/chat/dto/response-chat.dto.ts
+++ src/domain/chat/dto/response-chat.dto.ts
@@ -2,10 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseChatDto {
   @ApiProperty({
-    example: '김철수',
-    description: '가족 구성원 이름',
+    example: '푸앙이',
+    description: '가족 구성원 별명',
   })
-  familyMemberName: string;
+  nickname: string;
   @ApiProperty({
     example: 1,
     description: '가족 구성원 역할',
@@ -16,4 +16,9 @@ export class ResponseChatDto {
     description: '채팅 내용',
   })
   message: string;
+  @ApiProperty({
+    example: '12:00',
+    description: '채팅 생성 시간',
+  })
+  createdTime: string;
 }
