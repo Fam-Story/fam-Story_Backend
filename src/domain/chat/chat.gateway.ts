1cc917aa578aea532f27f7aea58c5abe1a2428de
Ahn Jiwan <synoti21@naver.com>
2023-12-04 18:56:04 +0900

    :bug: fix: Chat Dto JSON 형식으로 인식하도록 수정 (#66)

--- src/domain/chat/chat.gateway.ts
+++ src/domain/chat/chat.gateway.ts
@@ -8,12 +8,9 @@ import {
 import { Server, Socket } from 'socket.io';
 import { UseGuards } from '@nestjs/common';
 import { JwtServiceAuthGuard } from '../../auth/guards/jwt-service-auth.guard';
-import { InjectRepository } from '@nestjs/typeorm';
-import { ChatMessage } from '../../infra/entities/message.entity';
 import { ChatService } from './chat.service';
 import { CreateChatDto } from './dto/create-chat.dto';
 
-@UseGuards(JwtServiceAuthGuard)
 @WebSocketGateway({
   namespace: 'chat',
   cors: true,
@@ -22,10 +19,7 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  constructor(
-    @InjectRepository(ChatMessage)
-    private readonly chatService: ChatService,
-  ) {}
+  constructor(private readonly chatService: ChatService) {}
 
   @SubscribeMessage('joinFamily')
   async handleJoinRoom(
@@ -38,17 +32,16 @@ export class ChatGateway {
 
   @SubscribeMessage('sendMessage')
   async handleMessage(
-    @MessageBody()
-    data: { createChatDto: CreateChatDto },
+    @MessageBody() createChatDto: CreateChatDto,
     @ConnectedSocket() client: Socket,
   ) {
     const createDate = new Date();
-    await this.chatService.saveChat(data.createChatDto, createDate);
+    await this.chatService.saveChat(createChatDto, createDate);
 
     // 다른 가족 구성원에게 메시지 전송
-    this.server.to(data.createChatDto.familyId).emit('receiveMessage', {
-      familyMemberId: data.createChatDto.familyMemberId,
-      message: data.createChatDto.message,
+    this.server.to(createChatDto.familyId).emit('receiveMessage', {
+      familyMemberId: createChatDto.familyMemberId,
+      message: createChatDto.message,
       createdAt: createDate, // 메시지가 저장된 시간
     });
   }
