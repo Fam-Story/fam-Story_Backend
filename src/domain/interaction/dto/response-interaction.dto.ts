80519d0fec3290caa154b5c2053a50a0d45b0a05
Ahn Jiwan <synoti21@naver.com>
2023-12-11 19:11:19 +0900

    :sparkles: feat: ResponseInteractionDto 수정 (#92)

--- src/domain/interaction/dto/response-interaction.dto.ts
+++ src/domain/interaction/dto/response-interaction.dto.ts
@@ -4,32 +4,38 @@ import { ApiProperty } from '@nestjs/swagger';
 export class ResponseInteractionDto {
   @ApiProperty({ example: 1, description: '상호작용 ID', nullable: false })
   readonly interactionId: number;
+
   @ApiProperty({
     example: 2,
     description: '상호작용을 보내는 유저 ID',
     nullable: false,
   })
   readonly srcMemberId: number;
+
   @ApiProperty({
     example: 3,
     description: '상호작용을 받는 유저 ID',
     nullable: false,
   })
   readonly dstMemberId: number;
+
   @ApiProperty({
     example: true,
     description: '상호작용을 확인했는지 여부',
     nullable: false,
   })
   readonly isChecked: boolean;
+
   @ApiProperty({ example: 1, description: '상호작용 타입', nullable: false })
   readonly interactionType: number;
+
   @ApiProperty({
     example: 1,
     description: '상호작용을 보낸 구성원의 닉네임',
     nullable: false,
   })
   readonly srcMemberNickname: string;
+
   @ApiProperty({
     example: 1,
     description: '상호작용을 보낸 구성원의 역할',
@@ -43,12 +49,16 @@ export class ResponseInteractionDto {
     dstMemberId: number,
     isChecked: boolean,
     interactionType: number,
+    srcMemberNickname: string,
+    srcMemberRole: number,
   ) {
     this.interactionId = interactionId;
     this.srcMemberId = srcMemberId;
     this.dstMemberId = dstMemberId;
     this.isChecked = isChecked;
     this.interactionType = interactionType;
+    this.srcMemberNickname = srcMemberNickname;
+    this.srcMemberRole = srcMemberRole;
   }
 
   static of(
@@ -57,6 +67,8 @@ export class ResponseInteractionDto {
     dstMemberId: number,
     isChecked: boolean,
     interactionType: number,
+    srcMemberNickname: string,
+    srcMemberRole: number,
   ): ResponseInteractionDto {
     return new ResponseInteractionDto(
       interactionId,
@@ -64,6 +76,8 @@ export class ResponseInteractionDto {
       dstMemberId,
       isChecked,
       interactionType,
+      srcMemberNickname,
+      srcMemberRole,
     );
   }
 
@@ -74,6 +88,8 @@ export class ResponseInteractionDto {
       interaction.dstMember.id,
       interaction.isChecked,
       interaction.interactionType,
+      interaction.nickname,
+      interaction.role,
     );
   }
 }
